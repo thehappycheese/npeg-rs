743779ccb4f5a4d55599a7ca1b7af812b4662837
thehappycheese <the.nicholas.archer@gmail.com>
2022-09-30 00:27:39 +0800

    added thoughts

--- src/ops/parser.rs
+++ src/ops/parser.rs
@@ -14,9 +14,26 @@ impl<'ft> ParseContext<'ft>{
     fn get_full_text(&self)->&str {
         self.full_text
     }
-    fn get_prior_result(&self, parser_operator:&'ft ParserOperator, start_position:usize)->Option<&ParserMatch>{
+    fn get_memory(&self, parser_operator:&'ft ParserOperator, start_position:usize)->Option<&ParserMatch>{
         self.memory.get(&(parser_operator, start_position))
     }
+    fn set_memory(&self, parser_operator:&'ft ParserOperator, start_position:usize, parser_match:ParserMatch){
+        // TODO: when .insert succeeds the key is not updated
+        //       this can be a problem for types that can be == without being the same instance.
+        //       (our grammar may include several == subtrees which are NOT the same instances
+        //        ideally we would de-duplicate these subtrees before calling parse... but lets say it's not guaranteed for now...)
+        //       `key(parser_operator1,..) == key(parser_operator2,..)` is the desired behavior, HOWEVER
+        //       it is preferable to check `key(parser_operator1,..) is key(parser_operator2,..)`,
+        //       rather than doing all the work for a deeply nested == comparison
+        
+        //       OK so it turns out this is only achievable using unsafe raw pointers. Soooo that sucks.
+        if let Some(_old_value) = self.memory.insert((parser_operator, start_position), parser_match){
+            // TODO: If we try re-insert over the same key, this is probably not the user's fault, is it recoverable with nice message?
+            panic!("Reinserted over same memory")
+        }
+        // TODO: every time the parser steps forward, we can probably clear parts of this map where start_position < new_position
+    }
+
     fn push_rule_set(&self, rule_set:&'ft ParserRuleSet){
         self.rule_sets.push(rule_set)
     }
@@ -56,11 +73,14 @@ impl Parser for ParserOperator {
             ParserOperator::RuleReference { rule_name } => {
                 if let Some(rule) = context.get_rule(rule_name){
                     rule.parse(context, start_position)
+                }else{
+                    // TODO: Probably the user would like a nice message, not a crash
+                    panic!("Invalid rule reference not found in current grammar")
                 }
             }
 
             ParserOperator::Literal { literal_text } => {
-                if full_text[start_position..].starts_with(literal_text) {
+                if context.full_text[start_position..].starts_with(literal_text) {
                     Some(ParserMatch {
                         start: start_position,
                         end: start_position + literal_text.len(),
@@ -73,7 +93,7 @@ impl Parser for ParserOperator {
             }
 
             ParserOperator::Regex { regular_expression } => {
-                let text_to_match = &full_text[start_position..];
+                let text_to_match = &context.full_text[start_position..];
                 regular_expression.find(text_to_match).map(|re_match| {
                     if re_match.start() != 0 {
                         panic!("Regular expression matched but not at the specified position")
@@ -93,7 +113,7 @@ impl Parser for ParserOperator {
                 let mut end_position = start_position;
                 let sub_matches: Vec<ParserMatch> = children
                     .iter()
-                    .map_while(|child| match child.parse(full_text, end_position) {
+                    .map_while(|child| match child.parse(context, end_position) {
                         Some(ma) => {
                             end_position += ma.len();
                             Some(ma)
@@ -119,7 +139,7 @@ impl Parser for ParserOperator {
                 children
                     .iter()
                     .find_map(|child| {
-                        child.parse(full_text, end_position).map(|ma| {
+                        child.parse(context, end_position).map(|ma| {
                             end_position += ma.len();
                             ma
                         })
@@ -139,7 +159,7 @@ impl Parser for ParserOperator {
             } => {
                 let mut end_position = start_position;
                 let sub_matches: Vec<ParserMatch> = (0..*maximum_occurrences)
-                    .map_while(|_| match child.parse(full_text, end_position) {
+                    .map_while(|_| match child.parse(context, end_position) {
                         Some(ma) => {
                             end_position += ma.len();
                             Some(ma)
@@ -162,11 +182,11 @@ impl Parser for ParserOperator {
 
             ParserOperator::Lookahead { child, scout, accept_match } => {
                 let mut end_position = start_position;
-                let res_child = child.parse(full_text, start_position).map(|parser_match| {
+                let res_child = child.parse(context, start_position).map(|parser_match| {
                     end_position += parser_match.len();
                     parser_match
                 });
-                let res_scout = scout.parse(full_text, end_position);
+                let res_scout = scout.parse(context, end_position);
                 if (*accept_match && res_scout.is_some()) || (!accept_match && res_scout.is_none()) {
                     res_child
                 } else {
